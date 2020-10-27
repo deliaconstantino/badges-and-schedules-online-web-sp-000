2a28ebe89c4ac39607c27cc26941c3cae2d33f7c
Delia Constantino <dconstan92@gmail.com>
2020-10-27 20:13:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,16 +6,16 @@ def batch_badge_creator(attendees)
   attendees.collect{|name| "Hello, my name is #{name}."}
 end
 
-# def assign_rooms(attendees)
-#   attendees.map.with_index{|name, i| "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
-# end
-
 def assign_rooms(attendees)
-  count = 1
-  room_assignment_array = []
-  attendees.each do |name|
-    room_assignment_array.push("Hello, #{name}! You'll be assigned to room #{count}!")
-    count +=1
-  end
-  room_assignment_array
+  attendees.map.with_index{|name, i| "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
 end
+
+# def assign_rooms(attendees)
+#   count = 1
+#   room_assignment_array = []
+#   attendees.each do |name|
+#     room_assignment_array.push("Hello, #{name}! You'll be assigned to room #{count}!")
+#     count +=1
+#   end
+#   room_assignment_array
+# end
