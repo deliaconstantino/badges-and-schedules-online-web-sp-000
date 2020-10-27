00eac4a0622c334bbaf86ead257a70f8287016ec
Delia Constantino <dconstan92@gmail.com>
2020-10-27 19:58:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-# def batch_badge_creator(attendees)
-#   badges = []
-#   attendees.each do |name|
-#     badges << "Hello, my name is #{name}."
-#   end
-#   badges
-# end
-
 def batch_badge_creator(attendees)
   attendees.collect{|name| "Hello, my name is #{name}."}
 end
