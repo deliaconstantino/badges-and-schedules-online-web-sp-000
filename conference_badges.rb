89676a949560dd64c863c04d9bd03908066501ec
Delia Constantino <dconstan92@gmail.com>
2020-10-27 20:44:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,17 +11,6 @@ def assign_rooms(attendees)
   attendees.map.with_index{|name, i| "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
 end
 
-# def printer(attendees)
-#   name_phrase = batch_badge_creator(attendees)
-#   name_phrase.each do |phrase|
-#     puts phrase
-#   end
-#   room_phrase = assign_rooms(attendees)
-#   room_phrase.each do |phrase_two|
-#     puts phrase_two
-#   end
-# end
-
 def printer(attendees)
   batch_badge_creator(attendees).each do |name_phrase|
     puts name_phrase
