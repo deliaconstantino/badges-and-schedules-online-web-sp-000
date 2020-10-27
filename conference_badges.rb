74bb132949d4e5975c28c977f6c59c7c0abf3e77
Delia Constantino <dconstan92@gmail.com>
2020-10-27 20:11:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ end
 # def assign_rooms(attendees)
 #   attendees.map.with_index{|name, i| "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
 # end
+
+def assign_rooms(attendees)
+  count = 1
+  room_assigment_array = []
+  attendees.each do |name|
+    room_assignment_array.push("")
+  end
+end
